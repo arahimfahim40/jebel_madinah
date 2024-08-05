2c33535ceb4c129fb4bd781292b46d0f25079d4d
azizpgl <aziznabizada.mailbox@gmail.com>
2024-08-05 20:38:11 +0430

    changes applied on user section

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Http\Controllers\Admin;
+
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -11,34 +13,39 @@ use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    public function index(Request $request){
+    public function index(Request $request)
+    {
 
         $paginate = $request->paginate ? $request->paginate : 10;
         $users = User::when(!empty($request->searchValue), function ($q) use ($request) {
             $q->where('name', 'LIKE', "%$request->searchValue%");
             $q->orWhere('username', 'LIKE', "%$request->searchValue%");
             $q->orWhere('email', 'LIKE', "%$request->searchValue%");
-        })->when(!empty($request->user_status), function ($q) use ($request){
-            $q->where('status',$request->user_status);
-        })->with(['time_zones','createdBy','updatedBy'])->orderBy('id', 'desc')->paginate($paginate);
+        })->when(!empty($request->user_status), function ($q) use ($request) {
+            $q->where('status', $request->user_status);
+        })->where('email', '!=', 'admin@gmail.com')
+            ->with(['time_zones', 'createdBy', 'updatedBy'])
+            ->orderBy('id', 'desc')
+            ->paginate($paginate);
         if ($request->ajax()) {
             return view('admin.users.data', compact('users',  'paginate'));
         }
         return view('admin.users.list', compact('users',  'paginate'));
     }
 
-    public function create(){
+    public function create()
+    {
         $timezones = DB::table('time_zones')->get();
         $roles = DB::table('roles')->get();
         $perms = DB::table('permissions')->orderBy('group_name')->get();
         $permissions = array();
-        foreach ($perms as $p){
-            if(!array_key_exists($p->group_name, $permissions))
-                $permissions[$p->group_name] = [$p->id=>$p->name];
-            else 
+        foreach ($perms as $p) {
+            if (!array_key_exists($p->group_name, $permissions))
+                $permissions[$p->group_name] = [$p->id => $p->name];
+            else
                 $permissions[$p->group_name][$p->id] = $p->name;
         }
-        return view('admin.users.create', compact('timezones','roles','permissions'));
+        return view('admin.users.create', compact('timezones', 'roles', 'permissions'));
     }
 
     public function store(Request $request)
@@ -54,7 +61,7 @@ class UserController extends Controller
             'roles.*' => 'exists:roles,id',
             'permissions.*' => 'exists:permissions,id',
         ]);
-        
+
         $user = Auth::user();
         try {
             $user = User::create([
@@ -64,10 +71,10 @@ class UserController extends Controller
                 "time_zone_id" => $request['time_zone_id'],
                 "password" => bcrypt($request['password']),
                 "status" => $request["status"],
-                "created_by" =>$user->id,
-                "updated_by" =>$user->id,
+                "created_by" => $user->id,
+                "updated_by" => $user->id,
             ]);
-            if($request['roles']){
+            if ($request['roles']) {
                 $user->roles()->sync([$request['roles']]);
             }
             $user->permissions()->sync($request['permissions']);
@@ -77,56 +84,59 @@ class UserController extends Controller
         }
     }
 
-    public function edit($id){
-        
-        $timezones = DB::table('time_zones')->get();
-        $roles = DB::table('roles')->get();
-        $perms = DB::table('permissions')->orderBy('group_name')->get();
-        $permissions = array();
-
-        foreach ($perms as $p){
-            if(!array_key_exists($p->group_name, $permissions))
-                $permissions[$p->group_name] = [$p->id=>$p->name];
-            else
-                $permissions[$p->group_name][$p->id] = $p->name;
-        }
-
-        $user = User::with(['time_zones:id,name','permissions','roles'])->find($id);
-        $has_permissions = [];
-        foreach($user->permissions as $p){
-            array_push($has_permissions, $p->id);
-        }
-
-        return view('admin.users.update',compact('user','timezones','roles','permissions','has_permissions'));
-    }
-
-    public function show($id){
+    public function edit($id)
+    {
 
         $timezones = DB::table('time_zones')->get();
         $roles = DB::table('roles')->get();
         $perms = DB::table('permissions')->orderBy('group_name')->get();
         $permissions = array();
 
-        foreach ($perms as $p){
-            if(!array_key_exists($p->group_name, $permissions))
-                $permissions[$p->group_name] = [$p->id=>$p->name];
+        foreach ($perms as $p) {
+            if (!array_key_exists($p->group_name, $permissions))
+                $permissions[$p->group_name] = [$p->id => $p->name];
             else
                 $permissions[$p->group_name][$p->id] = $p->name;
         }
 
-        $user = User::with(['time_zones:id,name','permissions','roles'])->find($id);
+        $user = User::with(['time_zones:id,name', 'permissions', 'roles'])->find($id);
         $has_permissions = [];
-        foreach($user->permissions as $p){
+        foreach ($user->permissions as $p) {
             array_push($has_permissions, $p->id);
         }
-        return view('admin.users.view',compact('user','timezones','roles','permissions','has_permissions'));
+
+        return view('admin.users.update', compact('user', 'timezones', 'roles', 'permissions', 'has_permissions'));
     }
 
-    public function update(Request $request, $id){
+    public function show($id)
+    {
+
+        $timezones = DB::table('time_zones')->get();
+        $roles = DB::table('roles')->get();
+        $perms = DB::table('permissions')->orderBy('group_name')->get();
+        $permissions = array();
+
+        foreach ($perms as $p) {
+            if (!array_key_exists($p->group_name, $permissions))
+                $permissions[$p->group_name] = [$p->id => $p->name];
+            else
+                $permissions[$p->group_name][$p->id] = $p->name;
+        }
+
+        $user = User::with(['time_zones:id,name', 'permissions', 'roles'])->find($id);
+        $has_permissions = [];
+        foreach ($user->permissions as $p) {
+            array_push($has_permissions, $p->id);
+        }
+        return view('admin.users.view', compact('user', 'timezones', 'roles', 'permissions', 'has_permissions'));
+    }
+
+    public function update(Request $request, $id)
+    {
         $this->validate($request, [
             'name' => 'required|string',
-            'username' => 'required|unique:customers,email|unique:users,username,'.$id.'',
-            'email' => 'required|unique:customers,email|unique:users,email,'.$id,
+            'username' => 'required|unique:customers,email|unique:users,username,' . $id . '',
+            'email' => 'required|unique:customers,email|unique:users,email,' . $id,
             'password' => 'nullable',
             'status' => 'required|string|in:Active,Inactive',
             'time_zone_id' => 'required|exists:time_zones,id',
@@ -137,38 +147,38 @@ class UserController extends Controller
         $user = Auth::user();
         try {
             $user = User::findOrFail($id);
-            $password = $request['password']?[ "password" => bcrypt($request['password'])]: [];
-            $user->update(array_merge( [
+            $password = $request['password'] ? ["password" => bcrypt($request['password'])] : [];
+            $user->update(array_merge([
                 "name" => $request['name'],
                 "username" => $request['username'],
                 "email" => $request['email'],
                 "time_zone_id" => $request['time_zone_id'],
                 "status" => $request["status"],
-                "updated_by" =>$user->id,
-            ],$password));
+                "updated_by" => $user->id,
+            ], $password));
 
             $roles = [];
-           if($request['roles'])$roles = [$request['roles']];
-            
+            if ($request['roles']) $roles = [$request['roles']];
+
             $user->roles()->sync($roles);
             $user->permissions()->sync($request['permissions']);
-            
+
             return redirect()->route('users.show', ['id' => $id])->with('success', 'Saved successfully!');
         } catch (Exception $ex) {
             return redirect()->route('users.show', ['id' => $id])->with('error', 'Something went wrong, cannot save the user.');
         }
     }
 
-    public function destroy($id){
+    public function destroy($id)
+    {
         $user = Auth::user();
-        if($user->id == $id) return redirect()->route('users.show', ['id' => $id])->with('error', 'Something went wrong, cannot delete the user.');
-        try{
-            User::where('id',$id)->update(['deleted_by' => $user->id]);
+        if ($user->id == $id) return redirect()->route('users.show', ['id' => $id])->with('error', 'Something went wrong, cannot delete the user.');
+        try {
+            User::where('id', $id)->update(['deleted_by' => $user->id]);
             User::find($id)->delete();
             return redirect()->route('users.index')->with('success', 'Deleted successfully!');
-        } catch(Exception $ex) {
+        } catch (Exception $ex) {
             return redirect()->route('users.show', ['id' => $id])->with('error', 'Something went wrong, cannot delete the user.');
         }
     }
 }
-?>
